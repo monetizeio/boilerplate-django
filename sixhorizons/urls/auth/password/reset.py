3341afc5bdb9896d1706ec11aed972c5c0dad960
Mark Friedenbach <mark@monetize.io>
2011-12-14 01:00:57 -0800

    Split code across multiple lines for aesthetic reasons.

--- sixhorizons/urls/auth/password/reset.py
+++ sixhorizons/urls/auth/password/reset.py
@@ -32,7 +32,8 @@
 
 from django.conf.urls.defaults import patterns, url
 from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth.views import password_reset, password_reset_done, \
+  password_reset_confirm, password_reset_complete
 from django.contrib.auth.tokens import default_token_generator
 
 urlpatterns = patterns('',
